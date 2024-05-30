43d9a32b58570dccc111f45d0627a1f3db441a4e
Jakob Hoeg Mørk <jakmoe01@easv365.dk>
2024-05-30 19:25:36 +0200

    added opengraph image + metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,51 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { WebLLMProvider } from "@/providers/web-llm-provider";
+import { title } from "process";
+
+const metainfo = {
+  name: "Chatty",
+  title: "Chatty",
+  description: "Chat with web-llm models in the browser",
+  url: "https://chattyui.com",
+  icons: {
+    icon: "/favicon-32x32.png",
+  },
+  image: "https://chattyui.com/metaimg.jpg",
+};
 
 export const metadata: Metadata = {
-  title: "WebChat",
-  description: "Chat with web-llm models in the browser",
+  metadataBase: new URL(metainfo.url),
+  title: {
+    default: metainfo.name,
+    template: "%s - " + metainfo.name,
+  },
+  description: metainfo.description,
+  authors: [
+    {
+      name: "Jakob Hoeg Mørk",
+      url: "https://jakobhoeg.dev",
+    },
+    {
+      name: "Addy Osmani",
+      url: "https://addyosmani.com",
+    },
+  ],
+  openGraph: {
+    type: "website",
+    title: metainfo.name,
+    url: metainfo.url,
+    description: metainfo.description,
+    images: [metainfo.image],
+    siteName: metainfo.name,
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: metainfo.url,
+    title: metainfo.name,
+    description: metainfo.description,
+    images: [metainfo.image],
+  },
 };
 
 export const viewport = {
