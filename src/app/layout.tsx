d21dda7b04d7f0326c99b38b16674c06b14c2729
Christian Oliff <christianoliff@pm.me>
2024-06-06 15:52:29 +0900

    Remove maximum-scale from viewport meta tag
    This can cause issues with accessibilty - and is ignored by modern browsers anyway.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,8 +54,6 @@ export const metadata: Metadata = {
 export const viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: 1,
 };
 
 export default function RootLayout({
