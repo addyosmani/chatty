346b7d6b25c78cbe2f10ecd6f3cfa189ce349ef1
Jakob Hoeg <jakmoe01@easv365.dk>
2025-01-07 12:50:29 +0100

    refactor: chat-bottombar

--- src/components/chat/chat-bottombar.tsx
+++ src/components/chat/chat-bottombar.tsx
@@ -21,6 +21,7 @@ import { Mic, Send, SendHorizonal } from "lucide-react";
 import useSpeechToText from "@/hooks/useSpeechRecognition";
 import MultiImagePicker from "../image-embedder";
 import { Models } from "@/lib/models";
+import { ChatInput } from "../ui/chat/chat-input";
 
 interface MergedProps extends ChatProps {
   files: File[] | undefined;
@@ -85,16 +86,31 @@ export default function ChatBottombar({
   }, [isLoading]);
 
   return (
-    <div className="p-1 flex justify-between w-full items-center gap-2">
+    <div className="px-4 pb-7 flex justify-between w-full items-center relative ">
       <AnimatePresence initial={false}>
-        <div className="w-full relative mb-2 items-center">
-          <div className="flex flex-col relative w-full bg-accent dark:bg-card rounded-lg">
-            <div className="flex w-full">
-              <form
-                onSubmit={handleSubmit}
-                className="w-full items-center flex relative gap-2"
-              >
-                <div className="absolute flex left-3 z-10">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full items-center flex flex-col  bg-accent dark:bg-card rounded-lg "
+        >
+          <ChatInput
+            autoComplete="off"
+            value={
+              isListening ? (transcript.length ? transcript : "") : input
+            }
+            ref={inputRef}
+            onKeyDown={handleKeyPress}
+            onChange={handleInputChange}
+            name="message"
+            placeholder={!isListening ? "Enter your prompt here" : "Listening"}
+            className="max-h-40 px-6 pt-6 border-0 shadow-none bg-accent rounded-lg text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed dark:bg-card"
+          />
+
+
+          <div className="flex w-full items-center p-2">
+            {isLoading ? (
+              // Loading state
+              <div className="flex w-full justify-between">
+                <div className="flex">
                   <MultiImagePicker disabled={selectedModel.name !== Models[6].name} onImagesPick={setBase64Images} />
                   <FileLoader
                     setFileText={setFileText}
@@ -102,117 +118,113 @@ export default function ChatBottombar({
                     setFiles={setFiles}
                   />
                 </div>
-                <TextareaAutosize
-                  autoComplete="off"
-                  value={
-                    isListening ? (transcript.length ? transcript : "") : input
-                  }
-                  ref={inputRef}
-                  onKeyDown={handleKeyPress}
-                  onChange={handleInputChange}
-                  name="message"
-                  placeholder={
-                    !isListening ? "Enter your prompt here" : "Listening"
-                  }
-                  className=" max-h-24 px-24 bg-accent py-[22px] rounded-lg  text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 w-full flex items-center h-16 resize-none overflow-hidden dark:bg-card"
-                />
-
-                {!isLoading ? (
-                  <div className="flex absolute right-3 items-center">
-                    {isListening ? (
-                      <div className="flex">
-                        <Button
-                          className="shrink-0 relative rounded-full bg-blue-500/30 hover:bg-blue-400/30 "
-                          variant="ghost"
-                          size="icon"
-                          type="button"
-                          onClick={handleListenClick}
-                          disabled={isLoading}
-                        >
-                          <Mic className="w-5 h-5 " />
-                          <span className="animate-pulse absolute h-[120%] w-[120%] rounded-full bg-blue-500/30" />
-                        </Button>
-                      </div>
-                    ) : (
-                      <Button
-                        className="shrink-0 rounded-full"
-                        variant="ghost"
-                        size="icon"
-                        type="button"
-                        onClick={handleListenClick}
-                        disabled={isLoading}
-                      >
-                        <Mic className="w-5 h-5 " />
-                      </Button>
+                <div>
+                  <Button
+                    className="shrink-0 rounded-full"
+                    variant="ghost"
+                    size="icon"
+                    type="button"
+                    disabled
+                  >
+                    <Mic className="w-5 h-5" />
+                  </Button>
+                  <Button
+                    className="shrink-0 rounded-full"
+                    variant="ghost"
+                    size="icon"
+                    type="submit"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      stop();
+                    }}
+                  >
+                    <StopIcon className="w-5 h-5" />
+                  </Button>
+                </div>
+              </div>
+            ) : (
+              // Default state
+              <div className="flex w-full justify-between">
+                <div className="flex">
+                  <MultiImagePicker disabled={selectedModel.name !== Models[6].name} onImagesPick={setBase64Images} />
+                  <FileLoader
+                    setFileText={setFileText}
+                    files={files}
+                    setFiles={setFiles}
+                  />
+                </div>
+                <div>
+                  {/* Microphone button with animation when listening */}
+                  <Button
+                    className={`shrink-0 rounded-full ${isListening
+                      ? "relative bg-blue-500/30 hover:bg-blue-400/30"
+                      : ""
+                      }`}
+                    variant="ghost"
+                    size="icon"
+                    type="button"
+                    onClick={handleListenClick}
+                    disabled={isLoading}
+                  >
+                    <Mic className="w-5 h-5" />
+                    {isListening && (
+                      <span className="animate-pulse absolute h-[120%] w-[120%] rounded-full bg-blue-500/30" />
                     )}
-                    <Button
-                      className="shrink-0 rounded-full"
-                      variant="ghost"
-                      size="icon"
-                      type="submit"
-                      disabled={isLoading || !input.trim() || isListening}
-                    >
-                      <SendHorizonal className="w-5 h-5 " />
-                    </Button>
-                  </div>
-                ) : (
-                  <div className="flex absolute right-3 items-center">
-                    <Button
-                      className="shrink-0 rounded-full"
-                      variant="ghost"
-                      size="icon"
-                      type="button"
-                      disabled={true}
-                    >
-                      <Mic className="w-5 h-5 " />
-                    </Button>
-                    <Button
-                      className="shrink-0 rounded-full"
-                      variant="ghost"
-                      size="icon"
-                      type="submit"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        stop();
-                      }}
-                    >
-                      <StopIcon className="w-5 h-5  " />
-                    </Button>
-                  </div>
-                )}
+                  </Button>
 
-              </form>
-            </div>
-            {base64Images && (
-              <div className="flex px-2 pb-2 gap-2 ">
-                {base64Images.map((image, index) => {
-                  return (
-                    <div key={index} className="relative bg-muted-foreground/20 flex w-fit flex-col gap-2 p-1 border-t border-x rounded-md">
-                      <div className="flex text-sm">
-                        <Image src={image} width={20} height={20}
-                          className="h-auto rounded-md w-auto max-w-[100px] max-h-[100px]" alt={"Selected images"} />
-                      </div>
-                      <Button
-                        onClick={() => {
-                          const updatedImages = (prevImages: string[]) => prevImages.filter((_, i) => i !== index);
-                          setBase64Images(updatedImages(base64Images));
-                        }}
-                        size='icon' className="absolute -top-1.5 -right-1.5 text-white cursor-pointer  bg-red-500 hover:bg-red-600 w-4 h-4 rounded-full flex items-center justify-center">
-                        <Cross2Icon className="w-3 h-3" />
-                      </Button>
-                    </div>
-                  )
-                })}
+                  {/* Send button */}
+                  <Button
+                    className="shrink-0 rounded-full"
+                    variant="ghost"
+                    size="icon"
+                    type="submit"
+                    disabled={
+                      isLoading ||
+                      !input.trim() ||
+                      isListening ||
+                      !selectedModel
+                    }
+                  >
+                    <SendHorizonal className="w-5 h-5" />
+                  </Button>
+                </div>
               </div>
             )}
           </div>
-          <div className="w-full flex justify-center text-center px-10 md:px-0">
-            <p className="text-xs pt-2 text-muted-foreground">
-              The first response might take a little longer to process as the
-              model is being downloaded.
-            </p>
-          </div>
-        </div>
+          {base64Images && (
+            <div className="w-full flex px-2 pb-2 gap-2 ">
+              {base64Images.map((image, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="relative bg-muted-foreground/20 flex w-fit flex-col gap-2 p-1 border-t border-x rounded-md"
+                  >
+                    <div className="flex text-sm">
+                      <Image
+                        src={image}
+                        width={20}
+                        height={20}
+                        className="h-auto rounded-md w-auto max-w-[100px] max-h-[100px]"
+                        alt={""}
+                      />
+                    </div>
+                    <Button
+                      onClick={() => {
+                        const updatedImages = (prevImages: string[]) =>
+                          prevImages.filter((_, i) => i !== index);
+                        setBase64Images(updatedImages(base64Images));
+                      }}
+                      size="icon"
+                      className="absolute -top-1.5 -right-1.5 text-white cursor-pointer  bg-red-500 hover:bg-red-600 w-4 h-4 rounded-full flex items-center justify-center"
+                    >
+                      <Cross2Icon className="w-3 h-3" />
+                    </Button>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </form>
       </AnimatePresence>
     </div>
   );
